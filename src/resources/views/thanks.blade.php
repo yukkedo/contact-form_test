c4c610758db992ff1fe7f46e374e3fdb36fecb20
yurika tsuji <sakurasou.tystu@gmail.com>
2024-12-11 05:13:35 +0900

    view作成

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>お問い合わせ</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/thanks.css') }}" />
+</head>
+
+<body>
+    <main>
+        <div class="thanks">
+            <div class="back__text">
+                <h2>Thank you</h2>
+            </div>
+            <div class="thanks__form">
+                <div class="thanks__text">
+                    <h2>お問い合わせありがとうございました</h2>
+                </div>
+                <div class="thanks__button">
+                    <button class="thanks__button--submit" type="submit">
+                    HOME
+                    </button>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
